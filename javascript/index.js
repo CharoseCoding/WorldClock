b9613ce71ecfc16547c88dc7eb9ada835f8c7061
CharoseCoding <130360001+CharoseCoding@users.noreply.github.com>
2023-06-30 22:04:46 +0100

    add some java

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("dddd Do MMMM YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let torontoElement = document.querySelector("#toronto");
+  let torontoDateElement = torontoElement.querySelector(".date");
+  let torontoTimeElement = torontoElement.querySelector(".time");
+  let torontoTime = moment().tz("America/Toronto");
+
+  torontoDateElement.innerHTML = torontoTime.format("dddd Do MMMM YYYY");
+  torontoTimeElement.innerHTML = torontoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
